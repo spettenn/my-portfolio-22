565e8f0a4d84a9470f22e03096d3d19291587e79
spettenn <aleksanderspetalen@gmail.com>
2022-09-16 14:27:43 +0200

    text change with onClick works, just need to fix style and content for it

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
+function displayAgeYoung() {
+	document.getElementById('age').innerHTML = 'Hello there this is young age';
+}
+function displayAgeMiddle() {
+	document.getElementById('age').innerHTML = 'Hello there this is middle age';
+}
+function displayAgeLate() {
+	document.getElementById('age').innerHTML = 'Hello there this is later age';
+}
+
 export default function About() {
-	return <div className='about_page_container'>about</div>;
+	return (
+		<div className='about_page_container'>
+			<h1>About Page</h1>
+			<div className='about_btn_container'>
+				<button id='young_age_btn' type='button' onClick={displayAgeYoung}>
+					Age 1 - 10
+				</button>
+				<button id='middle_age_btn' type='button' onClick={displayAgeMiddle}>
+					Age 10 - 20
+				</button>
+				<button id='late_age_btn' type='button' onClick={displayAgeLate}>
+					Age 20 - Today
+				</button>
+			</div>
+			<p id='age'>This is default text, lets see if we can change it!</p>
+		</div>
+	);
 }
