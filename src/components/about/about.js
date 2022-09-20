ef7e250b9a5de1720bb50046c0567eedf1ff1c34
spettenn <69841778+spettenn@users.noreply.github.com>
2022-09-20 19:44:43 +0200

    some text and minor

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,29 +9,46 @@ function displayAgeYoung() {
 }
 function displayAgeMiddle() {
 	document.getElementById('age').style.backgroundColor = '#6C5B7B';
-	document.getElementById('age').innerHTML = 'Hello there this is middle age';
+	document.getElementById('age').innerHTML =
+		'during my youth i lived in Bærum, Norway. I was a pretty active child when i was younger, i continued my hobbies of playing fotball, basketball and snowboard during the winder. Sadly when i was 14 i had to stop with alot of sport due to a snowboarding incident where i hurt my back.' +
+		'This incident changed my life and my activities or hobbies had to be put on hold. So the next few years was spent in front of the computer, thus giving me the opportunity to understand and learn the web. During this time i also started to learn how to code, and this is where my interest in web-development started to grow.' +
+		'Later in my teens, i finished school (high school) and got a job at my old elementary school as a subtitute teacher. ';
 }
 function displayAgeLate() {
 	document.getElementById('age').style.backgroundColor = '#355C7D';
-	document.getElementById('age').innerHTML = 'Hello there this is later age';
+	document.getElementById('age').innerHTML =
+		'In my 20`s i started to study in Lillehammer, witch is a small town north of Oslo. During my time in Lillehammer i was studying tourism, this seemed (at the time) like a smart thing to study as the tourism industry was thriving in Norway at the time.' +
+		'I then finished my studies with a bachelors in marketing & tourism. After my studies, i spent alot of time trying to find a job. I ended up having alot of luck and having the privelige of working in Longyearbyen, Svalbard. This sadly did not last, as 4 months after i landed the jobb, covid-19 hit the shores of Norway and the country went in to lock-down. Thus i had to move back to the mainland and start looking for something new to do.' +
+		'After some time i realised that the tourism industri was not going to pick itself up after the initial covid-19 wave, so i started looking for a new trade.' +
+		'At this time i signed up for frontend web-development course for two years in Oslo. ';
 }
 
 export default function About() {
 	return (
 		<div id='about' className='about_page_container'>
 			<h1>About Page</h1>
-			<div className='about_btn_container'>
-				<button id='young_age_btn' type='button' onClick={displayAgeYoung}>
-					Age 1 - 10
-				</button>
-				<button id='middle_age_btn' type='button' onClick={displayAgeMiddle}>
-					Age 10 - 20
-				</button>
-				<button id='late_age_btn' type='button' onClick={displayAgeLate}>
-					Age 20 - Today
-				</button>
+			<div className='about_page_content'>
+				<div className='about_page_content_left'>
+					<div className='about_btn_container'>
+						<button id='young_age_btn' type='button' onClick={displayAgeYoung}>
+							Age 1 - 10
+						</button>
+						<button
+							id='middle_age_btn'
+							type='button'
+							onClick={displayAgeMiddle}>
+							Age 10 - 20
+						</button>
+						<button id='late_age_btn' type='button' onClick={displayAgeLate}>
+							Age 20 - Today
+						</button>
+					</div>
+					<p id='age'>Want to read trough a short summary of my life?</p>
+				</div>
+				<div className='about_page_content_right'>
+					<p>what can i do? </p>
+				</div>
 			</div>
-			<p id='age'>This is default text, lets see if we can change it!</p>
 		</div>
 	);
 }
