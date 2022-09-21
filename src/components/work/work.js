17b9a3cd0361ce7a3c3227540c5797814de84de0
spettenn <69841778+spettenn@users.noreply.github.com>
2022-09-21 16:53:36 +0200

    small commit

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -3,12 +3,16 @@ import React from 'react';
 export default function Work() {
 	return (
 		<div className='work_page_container'>
-			<div className='work_page_title'>
-				<h1>Work</h1>
-			</div>
-			<div className='work_page_content'>
+			<div className='work_top_content'>
 				<div className='work_page_content_left'>Previous project one</div>
 				<div className='work_page_content_right'>Previous project two</div>
+			</div>
+			<div className='work_bottom_content'>
+				<div class='work_card'>
+					<h4>where did i learn?</h4>
+				</div>
+				<div class='work_card'>What have i done?</div>
+				<div class='work_card'>What do i want to do?</div>
 			</div>
 		</div>
 	);
